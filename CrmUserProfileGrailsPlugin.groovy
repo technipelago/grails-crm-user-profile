b5df884f6e8c9d5f8dabfda6fa147603cbb6a6a8
Goran Ehrsson <goran@technipelago.se>
2017-02-02 22:42:52 +0100

    Version 2.4.2

--- CrmUserProfileGrailsPlugin.groovy
+++ CrmUserProfileGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmUserProfileGrailsPlugin {
     def groupId = ""
-    def version = "2.4.1"
+    def version = "2.4.2"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmSecurity']
@@ -50,34 +50,4 @@ This plugin provide user interface for managing user profiles (settings) in GR8 
         }
     }
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
