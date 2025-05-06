a7eb2873735855e76cfbf3bafb18db116393df6c
Goran Ehrsson <goran@technipelago.se>
2025-05-06 21:01:28 +0200

    feat: Update plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,8 +35,8 @@ grails.project.dependency.resolution = {
         test(":codenarc:0.25.2") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
-        compile ":crm-core:2.4.3"
-        compile ":crm-security:2.4.3"
+        compile ":crm-core:2.4.5"
+        compile ":crm-security:2.4.5"
     }
 }
 
